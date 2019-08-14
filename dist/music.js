4312a0669aa106bbec70d2a0fd904dcb4e77f273
叶添 <yet@nbpitech.com>
2019-08-14 10:37:58 +0800

    Site updated: 2019-08-14 10:37:55

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "来自天堂的魔鬼",
+        artist: '邓紫棋',
+        url: 'http://www.ytmp3.cn/down/33626.mp3',
+        cover: '',
+      }
+    ]
+});
